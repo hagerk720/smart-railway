2bc3df7fa0c0831edc8eba1dc19e6d9ced7ed41f
Hager <hagerk720@gmail.com>
2022-02-27 01:23:56 +0200

    first meeting

---
+++ lib/route/home.dart
@@ -0,0 +1,101 @@
+// ignore: file_names
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('smart railway'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image(
+              image: AssetImage("assets/images/img.png"),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Smart',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orange[600]),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Railway',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[500]),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 5),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your username',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 5),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your password',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 350,
+              child: ElevatedButton(
+                child: Text('Start' ),
+                onPressed: () {},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                    ),
+                    backgroundColor:
+                        (MaterialStateProperty.all<Color>(Colors.blue))),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
